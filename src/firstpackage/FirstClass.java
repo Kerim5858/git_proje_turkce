e528a224381e3022d252c28511e7e2053cf448f1
Kerim5858 <kerimanulur@gmail.com>
2020-06-20 14:35:10 -0400

    ikinci commit from locak repositoryden

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("first commit from local repositary");
+		System.out.println("second commit from local repository");
 	}
 
 }
